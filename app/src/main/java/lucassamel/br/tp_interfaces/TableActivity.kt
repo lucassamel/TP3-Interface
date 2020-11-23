846300567fcf2963cd46ab931cd86f1d36d9d897
Lucas Samel <lucas.samel97@gmail.com>
2020-11-23 16:18:27 -0300

    Initial commit

---
+++ app/src/main/java/lucassamel/br/tp_interfaces/TableActivity.kt
@@ -0,0 +1,11 @@
+package lucassamel.br.tp_interfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
